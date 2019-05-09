b9470969ffc97f628971a6ca1ae3cd88d9419c71
hamani97 <hamani@naver.com>
2019-05-09 17:38:20 +0900

    From office.. v1.0.7 - 버튼 이미지 추가

---
+++ sewing/src/main/java/com/suntech/iot/sewing/IntroActivity.kt
@@ -0,0 +1,12 @@
+package com.suntech.iot.sewing
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class IntroActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_intro)
+    }
+}
