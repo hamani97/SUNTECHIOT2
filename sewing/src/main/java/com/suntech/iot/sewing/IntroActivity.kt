d57b4aad9058dc26ff9488fb199d46841b56520b
hamani97 <hamani@naver.com>
2019-05-10 14:46:53 +0900

    From office.. cutting v1.0.7 sewing v1.0.0 - 일반재봉기 프로젝트 생성

--- sewing/src/main/java/com/suntech/iot/sewing/IntroActivity.kt
+++ sewing/src/main/java/com/suntech/iot/sewing/IntroActivity.kt
@@ -30,7 +30,7 @@ class IntroActivity : BaseActivity() {
             } else {
                 sendAppStartTime()
             }
-        }, 600)
+        }, 800)
     }
 
     private fun moveToNext() {
---
+++ sewing/src/main/java/com/suntech/iot/sewing/SettingActivity.kt
@@ -0,0 +1,13 @@
+package com.suntech.iot.sewing
+
+import android.os.Bundle
+import com.suntech.iot.sewing.base.BaseActivity
+
+class SettingActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_setting)
+//        initView()
+    }
+}
